0611fe9399f9ecef76c067099caf8b308092b4e9
j1m5s3 <james.lynch.uml@gmail.com>
2023-03-30 14:23:05 -0500

    Initial commit for docker...

--- routes/events/schemas/event_schemas.py
+++ routes/events/schemas/event_schemas.py
@@ -4,7 +4,10 @@ from marshmallow import Schema, fields
 class ContractSchema(Schema):
     contract_address = fields.Str(required=True)
     contract_abi = fields.List(fields.Dict, required=True)
+    price_mark = fields.Float(required=True)
     asset_symbol = fields.Str(required=True)
+    event_close = fields.Float(required=True)
+    betting_close = fields.Float(required=True)
 
     class Meta:
         unknown = 'exclude'
