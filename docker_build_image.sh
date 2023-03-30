b5041b718657ce51b4afd05f20d9f3105a7befe3
j1m5s3 <james.lynch.uml@gmail.com>
2023-03-30 15:04:08 -0500

    Added Docker functionality. Server can be run docker container...

--- docker_build_image.sh
+++ docker_build_image.sh
@@ -4,3 +4,8 @@ if [ $1 == "build" ]; then
     echo "Building container...";
     docker build -t over-under-api/v1 .;
 fi
+
+if [ $1 == "build-interactive" ]; then
+    echo "Building container...";
+    docker build -t over-under-api/v1:interactive -f Dockerfile-Interactive .;
+fi
--- docker_run_container.sh
+++ docker_run_container.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-docker run -t -d --network=host --expose 8000 over-inder-api/v1
+if [ $1 == "server" ]; then
+    echo "Running container as server...";
+    docker run -t -d --network=host --expose 8000 over-under-api/v1;
+fi
+
+if [ $1 == "run-interactive" ]; then
+    echo "Running container interactively...";
+    docker run -i -t over-under-api/v1:interactive sh;
+fi
